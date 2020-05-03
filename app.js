eb4cb77bd6b96390eef621c92367d90c7d05f93c
Hitesh <2016.hitesh.soneta@ves.ac.in>
2020-05-03 13:20:33 +0530

    authorization added

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
+const jwt = require("jsonwebtoken");
+
 require("dotenv/config");
 app.use(bodyparser.json());
 //import routes
 const route = require("./routes/routes");
-app.use("/car", route);
+app.use("/car", verifyToken, route, (req, res) => {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    }
+  });
+});
 //routes
 app.get("/", (req, res) => {
   res.send("home page");
@@ -15,6 +23,34 @@ app.get("/", (req, res) => {
 // mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
 //   console.log("connected to db!")
 // );
+app.post("/login", (req, res) => {
+  //dummy user
+  const user = {
+    id: 1,
+    username: "hitesh",
+  };
+  jwt.sign({ user }, "secretkey", (err, token) => {
+    res.json({ token });
+  });
+});
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers["authorization"];
+  // Check if bearer is undefined
+  if (typeof bearerHeader !== "undefined") {
+    // Split at the space
+    const bearer = bearerHeader.split(" ");
+    // Get token from array
+    const bearerToken = bearer[1];
+    // Set the token
+    req.token = bearerToken;
+    // Next middleware
+    next();
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+}
 
 mongoose.connect("mongodb://localhost/pandas", () =>
   console.log("connected to db!")
